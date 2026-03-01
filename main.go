9beee833260be10f2bde6c400e61f40dcad9367d
Swatantra <maverickswatantra@gmail.com>
2026-03-01 19:37:29 +0530

    fix: updated the postgres databse password

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 
 	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
-		dsn = "host=localhost user=postgres password=postgres dbname=contest_rating port=5432 sslmode=disable"
+		dsn = "host=localhost user=postgres password=user15 dbname=contest_rating port=5432 sslmode=disable"
 	}
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
